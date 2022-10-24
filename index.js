303798b84a46c0c33329e9ac2133cc634a1282bc
kamogawenka <kamogajosephine9@gmail.com>
2022-10-23 23:54:56 -0700

    my switch assignment

---
+++ index.js
@@ -0,0 +1,77 @@
+
+        let Elevator = 1
+        
+        switch(Elevator){
+           case 1:
+            console.log("Elevator 1") ="going to the first floor."
+            break;
+        
+            case 2:
+            console.log("Elevator 2") ="going to the second floor."
+            break;
+        
+            case 3:
+            console.log("elevator 3") ="going to the third floor."
+            break;
+        
+            
+            default:
+                console.log("imposible")
+        
+        
+        }
+        
+        
+        d
+
+
+
+
+
+
+
+            // console.log("testing")
+
+
+            //function
+
+            function add(){
+console.log(2+3)
+return 2+3
+
+            }
+            add()
+        
+  
+        function add(a,b){
+return a+b
+        }
+        const result =add(3,4)
+        console.log(result)
+
+        console
+
+        //class person
+        class person{
+constructor(name,age,gender){
+    this.name=name;
+    this.age=age;
+    this.gender=gender;
+}
+
+
+eat(patems){
+    console.log(params)
+}
+        }
+
+
+        let ruth= new person("ruth" 40,"female");
+        console.log(ruth)  
+
+        let jose= new person ("jose" ,20,"female");
+        console.log(jose)
+
+
+
+
